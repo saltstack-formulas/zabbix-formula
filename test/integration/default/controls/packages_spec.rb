3242c1469662ffc14368446df5eb11a140ebd2ea
Imran Iqbal <iqbalmy@hotmail.com>
2020-03-30 23:45:59 +0100

    test(packages_spec): update version numbers

--- test/integration/default/controls/packages_spec.rb
+++ test/integration/default/controls/packages_spec.rb
@@ -13,25 +13,25 @@ version =
   case platform[:name]
   when 'debian'
     if os[:release].start_with?('10')
-      '1:4.4.6-1+buster'
+      '1:4.4.7-1+buster'
     elsif os[:release].start_with?('9')
-      '1:4.4.6-1+stretch'
+      '1:4.4.7-1+stretch'
     elsif os[:release].start_with?('8')
-      '1:4.4.6-1+jessie'
+      '1:4.4.7-1+jessie'
     end
   when 'ubuntu'
     if os[:release].start_with?('18')
-      '1:4.4.6-1+bionic'
+      '1:4.4.7-1+bionic'
     elsif os[:release].start_with?('16')
-      '1:4.4.6-1+xenial'
+      '1:4.4.7-1+xenial'
     end
   when 'centos'
     if os[:release].start_with?('8')
-      '4.4.6-1.el8'
+      '4.4.7-1.el8'
     elsif os[:release].start_with?('7')
-      '4.4.6-1.el7'
+      '4.4.7-1.el7'
     elsif os[:release].start_with?('6')
-      '4.4.6-1.el6'
+      '4.4.7-1.el6'
     end
   when 'fedora'
     if os[:release].start_with?('31')
