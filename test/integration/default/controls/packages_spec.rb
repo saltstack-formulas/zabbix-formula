e4952f06f3e2c131a2beb2e30b56f6c3e7b4581a
Imran Iqbal <iqbalmy@hotmail.com>
2020-07-10 23:38:06 +0100

    test(packages_spec): generalise version number verification [skip ci]

--- test/integration/default/controls/packages_spec.rb
+++ test/integration/default/controls/packages_spec.rb
@@ -11,38 +11,12 @@ pkg_web =
   end
 version =
   case platform[:name]
-  when 'debian'
-    if os[:release].start_with?('10')
-      '1:4.4.9-1+buster'
-    elsif os[:release].start_with?('9')
-      '1:4.4.9-1+stretch'
-    elsif os[:release].start_with?('8')
-      '1:4.4.9-1+jessie'
-    end
-  when 'ubuntu'
-    if os[:release].start_with?('20')
-      '1:4.4.9-1+focal'
-    elsif os[:release].start_with?('18')
-      '1:4.4.9-1+bionic'
-    elsif os[:release].start_with?('16')
-      '1:4.4.9-1+xenial'
-    end
+  when 'debian', 'ubuntu'
+    '1:4.4'
   when 'centos'
-    if os[:release].start_with?('8')
-      '4.4.9-1.el8'
-    elsif os[:release].start_with?('7')
-      '4.4.9-1.el7'
-    elsif os[:release].start_with?('6')
-      '4.4.9-1.el6'
-    end
+    '4.4'
   when 'fedora'
-    if os[:release].start_with?('32')
-      '4.0.16-2.fc32'
-    elsif os[:release].start_with?('31')
-      '4.0.15-1.fc31'
-    elsif os[:release].start_with?('30')
-      '4.0.15-1.fc30'
-    end
+    '4.0'
   end
 
 control 'zabbix packages' do
@@ -55,7 +29,7 @@ control 'zabbix packages' do
   ].each do |p|
     describe package(p) do
       it { should be_installed }
-      its('version') { should eq version }
+      its('version') { should match(/^#{version}/) }
     end
   end
 end
